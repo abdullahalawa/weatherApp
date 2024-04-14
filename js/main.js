9ea12c69467b3767febb0b0ec1bf096888a347ec
Abdullah Alawa <awamah.group@gmail.com>
2024-04-15 01:36:56 +0300

    working on api function and on the input listner

--- js/main.js
+++ js/main.js
@@ -1,13 +1,36 @@
 "use strict";
 
+let cityNameInput = document.querySelector(".weather-Input");
+let cityNamedisplay = document.querySelector(".city-name");
+
 const apiKey = "076be003a33e4be784a141116241304";
 let cityName = "Damascus";
 let baseURL = `https://api.weatherapi.com/v1/forecast.json?q=${cityName}&key=${apiKey}&days=3`;
+let data = {};
 
-async function getWeatherData() {
-  let response = await fetch(baseURL);
-  let data = await response.json();
-  console.log(data);
+//getWeatherData through API
+async function getWeatherData(city) {
+  let response = await fetch(
+    `https://api.weatherapi.com/v1/forecast.json?q=${city}&key=${apiKey}&days=3`
+  );
+
+  if (response.status != 200) {
+    return response.status;
+  } else {
+    let finalData = await response.json();
+    return finalData;
+    // console.log(finalData.location.name);
+  }
 }
 
-getWeatherData();
+// getWeatherData("lon");
+
+cityNameInput.addEventListener("input", async function (e) {
+  data = await getWeatherData(cityNameInput.value);
+
+  if (data == 400) {
+    console.log("not valide city name");
+  } else {
+    cityNamedisplay.innerHTML = data.location.name;
+  }
+});
